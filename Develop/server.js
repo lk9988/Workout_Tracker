97133fdef9aba00de3f4fcd4a677e104e91308b5
lana <lanabkim@gmail.com>
2021-03-25 20:15:37 -0700

    fixing error

--- Develop/server.js
+++ Develop/server.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
 	useFindAndModify: false,
 	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 app.use(require("./routes/apiRoutes"));
